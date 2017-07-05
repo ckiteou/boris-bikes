779a49756debdcdd4eb9fa9e5d6dd2855b97bdf2
Costas Kiteou <ckiteou22@gmail.com>
2017-07-05 17:42:53 +0100

    Defined a full? predicate method that uses some of the dock code to return true or false depending on whether the station is full or not

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,23 @@ attr_reader :bikes
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Sorry, the dock is full" if @bikes.count >= 20
+    fail "Sorry, the dock is full" if full?
     @bikes << bike
   end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+
 end
