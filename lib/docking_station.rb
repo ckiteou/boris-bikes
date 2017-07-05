17e83c2b565568e74f96d938c661c68e5c637c72
Costas Kiteou <ckiteou22@gmail.com>
2017-07-05 16:32:52 +0100

    Used {} and raise_error syntax in RSpec unit test to test exception raising. Used fail keyword to raise an exception in code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,10 +4,11 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
-    bike
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,8 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
     bike = subject.release_bike
     expect(bike).to be_working
   end
@@ -12,7 +14,18 @@ describe DockingStation do
 
   it 'docks something' do
     bike = Bike.new
-    subject.dock(bike)
     expect(subject.dock(bike)).to eq bike
+  end
+
+  describe '#release_bike' do
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it "raises an error when there are no bikes available" do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
   end
 end
